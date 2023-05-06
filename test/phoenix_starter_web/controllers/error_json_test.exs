5e54ad0a86f45d3fa639eb24439d716383863040
Raj Rajhans <me@rajrajhans.com>
2023-05-06 17:18:14 +0530

    fix: ci errors (#2)
    * fix: ci
    
    * fix: cache key
    
    * fix: lint errors
    
    * fix: set db_url in runtime config for all envs
    
    * fix: db config in test
    
    * chore: add fly-deploy GH action
    
    * chore: debug
    
    * chore: debug db_url
    
    * chore: add env=test
    
    * chore: add .env
    
    * chore: update run clean
    
    * chore: update env
    
    * chore: add debug logs
    
    * chore: remove logs
    
    * temp: force mix instal
    
    * chore: revert forced install
    
    * chore: set mix_env
    
    * fix: db_url
    
    * chore: debug logs
    
    * chore: updatq
    
    * chore: attempt to fix ci failure
    
    * chore: debug log
    
    * chore: install rebar and hex in run_test.migration
    
    * chore: update .env.test
    
    * chore: remove debug logs

--- test/phoenix_starter_web/controllers/error_json_test.exs
+++ test/phoenix_starter_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule PhoenixStarterWeb.ErrorJSONTest do
   use PhoenixStarterWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert PhoenixStarterWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert PhoenixStarterWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
